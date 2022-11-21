d493ddce064c0c4e5a56b55932c011391b1b2f2c
Nikita Zmitrovich <47451179+s097t0r1@users.noreply.github.com>
2022-11-21 19:31:21 +0300

    build: add detekt ci cd action (#15)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    alias(libs.plugins.detekt)
+}
+
 buildscript {
     repositories {
         google()
@@ -7,11 +11,49 @@ buildscript {
         classpath(libs.kotlin.gradle.plugin)
         classpath(libs.android.gradle.plugin)
     }
-
 }
+
+
 allprojects {
+
     repositories {
         google()
         mavenCentral()
+    }
+
+    configureDetekt()
+
+}
+
+fun Project.configureDetekt() {
+
+    val reportMerge by tasks.registering(io.gitlab.arturbosch.detekt.report.ReportMergeTask::class) {
+        output.set(rootProject.buildDir.resolve("reports/detekt/merge.sarif"))
+    }
+
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+    detekt {
+        source = files("src/main/java", "src/main/kotlin")
+        parallel = true
+        buildUponDefaultConfig = true
+        config = files(
+            rootProject.file("config/detekt.yml"),
+            rootProject.file("config/detekt-compose.yml")
+        )
+
+        dependencies {
+            detektPlugins(rootProject.libs.kode.detekt.rules.compose)
+            detektPlugins(rootProject.libs.arturbosch.detekt.formatting)
+        }
+
+    }
+
+    plugins.withType(io.gitlab.arturbosch.detekt.DetektPlugin::class) {
+        tasks.withType(io.gitlab.arturbosch.detekt.Detekt::class) detekt@{
+            finalizedBy(reportMerge)
+            reportMerge.configure {
+                input.from(this@detekt.sarifReportFile)
+            }
+        }
     }
 }
